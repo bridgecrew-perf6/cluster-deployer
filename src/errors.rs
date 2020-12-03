d862777597d4bf737777819b0957245125d20caa
Esa Varemo <esa@kuivanto.fi>
2020-12-04 00:55:56 +0200

    Reimplement Error using a macro

--- src/errors.rs
+++ src/errors.rs
@@ -1,62 +1,40 @@
 use kube;
 use std::convert::From;
 
-impl From<std::io::Error> for KeyGenerationError {
-    fn from(err: std::io::Error) -> Self {
-        KeyGenerationError::IoError(err)
+macro_rules! generate_error {
+    { $name:ident: [$($type:ident: $content:ty,)+]} => {
+        #[derive(Debug)]
+        pub enum $name {
+            $(
+                $type($content),
+            )+
+        }
+
+        $(
+            impl From<$content> for $name {
+                fn from(err: $content) -> Self {
+                    $name::$type(err)
+                }
+            }
+        )+
     }
 }
 
-impl From<kube::Error> for KeyGenerationError {
-    fn from(err: kube::Error) -> Self {
-        KeyGenerationError::KubeError(err)
-    }
+generate_error! {
+    KeyGenerationError: [
+        IoError: std::io::Error,
+        KubeError: kube::Error,
+        CommandError: std::process::Output,
+    ]
 }
 
-#[derive(Debug)]
-pub enum KeyGenerationError {
-    IoError(std::io::Error),
-    KubeError(kube::Error),
-    CommandError(std::process::Output),
+generate_error! {
+    Error: [
+        KeyGenerationError: KeyGenerationError,
+        KubeError: kube::Error,
+        WatcherError: kube_runtime::watcher::Error,
+        JoinError: tokio::task::JoinError,
+        MultipleErrors: Vec<Error>,
+        SerdeJsonError: serde_json::Error,
+    ]
 }
-
-impl From<KeyGenerationError> for Error {
-    fn from(err: KeyGenerationError) -> Self {
-        Error::KeyGenerationError(err)
-    }
-}
-
-impl From<kube::Error> for Error {
-    fn from(err: kube::Error) -> Self {
-        Error::KubeError(err)
-    }
-}
-
-impl From<kube_runtime::watcher::Error> for Error {
-    fn from(err: kube_runtime::watcher::Error) -> Self {
-        Error::WatcherError(err)
-    }
-}
-
-impl From<tokio::task::JoinError> for Error {
-    fn from(err: tokio::task::JoinError) -> Self {
-        Error::JoinError(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::SerdeJsonError(err)
-    }
-}
-
-#[derive(Debug)]
-pub enum Error {
-    KeyGenerationError(KeyGenerationError),
-    KubeError(kube::Error),
-    WatcherError(kube_runtime::watcher::Error),
-    JoinError(tokio::task::JoinError),
-    MultipleErrors(Vec<Error>),
-    SerdeJsonError(serde_json::Error),
-}
-
