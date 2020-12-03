7036ddeca5489a36279a453deb1a1972672bef85
Esa Varemo <esa@kuivanto.fi>
2020-12-03 18:04:19 +0200

    Cleanup and refactoring

--- src/ssh.rs
+++ src/ssh.rs
@@ -42,37 +42,38 @@ fn generate_ssh_keypair() -> Result<KeyPair, KeyGenerationError> {
     })
 }
 
-/// Create SSH keypair if it doesn't exist
-/// Finished keypair will be stored in a k8s Secret.
-pub async fn ensure_ssh_key(namespace: &str) -> Result<(), Error> {
+/// Store given keypair in a secret
+async fn create_secret(api: &Api<Secret>, name: &str, keypair: KeyPair) -> Result<Secret, Error> {
+    let mut secret = Secret::default();
+    let mut data = std::collections::BTreeMap::new();
+    data.insert(String::from("private_key"), keypair.private_key);
+    data.insert(String::from("public_key"), keypair.public_key);
+    secret.string_data = Some(data);
+    secret.metadata.name = Some(name.into());
+
+    return api.create(&PostParams::default(), &secret).await
+        .map_err(|err| Error::KubeError(err));
+}
+
+/// Generate and store SSH key in a Secret if it doesn't exit
+pub async fn ensure_ssh_key(namespace: &str) -> Result<Secret, Error> {
     let client = Client::try_default().await?;
     let secret_api: Api<Secret> = Api::namespaced(client, namespace);
 
     match secret_api.get(KEY_NAME).await {
         Ok(key) => {
             println!("Key {} exists", KEY_NAME);
-            Ok(key)
+            return Ok(key);
         },
-        Err(kube::Error::Api(err)) => {
-          if err.reason == String::from("NotFound") {
-              println!("Key {} doesn't exists, creating", KEY_NAME);
-              let keypair = generate_ssh_keypair()?;
-
-              let mut secret = Secret::default();
-              let mut data = std::collections::BTreeMap::new();
-              data.insert(String::from("private_key"), keypair.private_key);
-              data.insert(String::from("public_key"), keypair.public_key);
-              secret.string_data = Some(data);
-              secret.metadata.name = Some(String::from(KEY_NAME));
-
-              secret_api.create(&PostParams::default(), &secret)
-                  .await
-                  .map_err(|err| Error::KubeError(err))
-          } else {
-              Err(Error::KubeError(kube::Error::Api(err)))
-          }
-        },
-        Err(err) => Err(err.into()),
-    }?;
-    Ok(())
+        Err(some_error) => {
+            if let kube::Error::Api(kube::error::ErrorResponse {reason, ..}) = &some_error {
+                if reason == &String::from("NotFound") {
+                    println!("Key {} doesn't exists, creating", KEY_NAME);
+                    let keypair = generate_ssh_keypair()?;
+                    let _secret = create_secret(&secret_api, KEY_NAME, keypair).await?;
+                }
+            }
+            return Err(some_error.into());
+        }
+    };
 }
